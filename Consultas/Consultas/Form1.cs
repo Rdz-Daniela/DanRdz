d458811557d541f3d15402de04fc405a45aa0d09
Rdz-Daniela <114102955+Rdz-Daniela@users.noreply.github.com>
2022-09-21 15:30:53 -0500

    Add files via upload

---
+++ Consultas/Consultas/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consultas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Registros = 0;
+            Conexion.EjecutaConsulta(textBox1.Text);
+            AccionesComunes.Mensaje("" + Registros);
+            AccionesComunes.LlenarCombo(textBox1.Text, comboBox1, "id_Cliente", "Nombre_Cliente");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
